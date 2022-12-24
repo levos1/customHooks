b15e14583079689f8ce71d8c769f1b5a3bd43ab1
levos1 <62560105+levos1@users.noreply.github.com>
2022-12-24 20:52:44 -0300

    subir custom hooks

---
+++ useTodo.js
@@ -0,0 +1,50 @@
+import { useReducer ,useEffect} from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+const init = ()=>{
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo= () =>{
+
+    const [todo, dispatchTodo] = useReducer(todoReducer, [],init)
+    useEffect(() => {
+      localStorage.setItem('todos',JSON.stringify(todo))
+        
+    }, [todo])
+    
+    const handleNewTodo = (todo) =>{
+        const action = {
+            type: "[TODO]: Add Todo",
+            payLoad: todo,
+        }
+        
+        dispatchTodo(action);
+    }
+    const handleDeleteTodo = (id)=>{
+        const action = {
+            type: "[TODO] Remove Todo",
+            payLoad: id,
+        }
+        dispatchTodo(action)
+    }
+    const handleToggleTodo = ( id) =>{
+        dispatchTodo( {
+            type: "[TODO] Toggle Todo",
+            payLoad: id,
+        })
+        //dispatchTodo(action)
+    }
+
+    return {
+        todo,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todoCount:todo.length,
+        pendingTodos:(todo.filter(todos =>todos.done===false).length)
+
+    }
+
+
+}
